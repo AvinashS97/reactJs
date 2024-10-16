ce0b1a4c916463f2651ac81b889069a1ec2c5a60
AvinashS97 <avikumar97170@gmail.com>
2024-10-16 12:59:14 +0530

    16 Oct

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// OR,  Behind the scene :-
+// import React from "react"
+// React.createElement('h1', null, "Hello, World!")
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// console.log(React.createElement('h1', null, "Hello, World!"))
+
+//? Each JSX expressions must have one parent element, which means if you try to return multiple elements, React will throw an error.
+
+//? Each JSX tags needs to be closed. You can use self-closing tags for elements that don't have children, eg., <img src="url" />
+
+
+export const App = () =>{
+  return <h2>Name: The Railway Man</h2>,
+  <h3>Rating: 8.5</h3>
+  <p>After a deadly gas leaks from a factory in Bhopal, brave railway workers risk their lives to save others in the face of an unspeakable disaster.</p>
 }
-
-export default App
