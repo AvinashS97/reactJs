fc90e3fde606e936fb3f2ae7d9c9769f423239b9
AvinashS97 <avikumar97170@gmail.com>
2024-10-17 16:44:11 +0530

    17 Oct

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,12 @@
 //? Each JSX tags needs to be closed. You can use self-closing tags for elements that don't have children, eg., <img src="url" />
 
 
+
+// React Fragments:- It allow components to return multiple elements without adding extra nodes to the DOM.
+      
+            // import { Fragment } from "react";
+            // return <Fragment>//</Fragment>
+
 export const App = () => {
 
   return (
@@ -24,20 +30,32 @@ export const App = () => {
   )
 };
 
+//Dynamic Values in JSX
+
+
+// Using Variables, Using Expressions
 const NetflixSeries = () => {
+  const name = "The Railway Men";
+  const rating= "8.5 / 10";
+  const summary = "After a deadly gas leaks from a factory in Bhopal, brave railway workers risk their lives to save others in the face of an unspeakable disaster.";
+
+
+
   return (
     <div>
       <div>
         <img src="The Railway Man jpeg.jfif" alt="movie" width="20%" height="10%" />
       </div>
-      <h2>Name: The Railway Man</h2>
-      <h3>Rating: 8.5</h3>
-      <p>Summary: After a deadly gas leaks from a factory in Bhopal, brave railway workers risk their lives to save others in the face of an unspeakable disaster.</p>
+      <h2>Name: {name} </h2>
+      <h3>Year: {2020+3} </h3>
+      <h3>Rating: {rating} </h3>
+      <p>Summary: {summary} </p>
     </div>
   )
 };
 
 
-      // React Fragments:- It allow components to return multiple elements without adding extra nodes to the DOM.
-            // import { Fragment } from "react";
-            // return <Fragment>//</Fragment>
+
+
+
+  
