61fe3942c6675b029d67e9120d79daac999ce368
AvinashS97 <avikumar97170@gmail.com>
2024-10-17 18:04:40 +0530

    Conditional Operator in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@
 // OR,  Behind the scene :-
 // import React from "react"
 // React.createElement('h1', null, "Hello, World!")
+// console.log(React.createElement('h1', null, "Hello, World!"))
+
 
 // import {Fragment } from "react";
 
-// console.log(React.createElement('h1', null, "Hello, World!"))
 
 //? Each JSX expressions must have one parent element, which means if you try to return multiple elements, React will throw an error.
 
@@ -13,7 +14,7 @@
 
 
 
-// React Fragments:- It allow components to return multiple elements without adding extra nodes to the DOM.
+//! React Fragments:- It allow components to return multiple elements without adding extra nodes to the DOM.
       
             // import { Fragment } from "react";
             // return <Fragment>//</Fragment>
@@ -23,23 +24,60 @@ export const App = () => {
   return (
     <>
       <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
+
+      {/* <NetflixSeries /> */}
+      {/* <NetflixSeries /> */}
+      {/* <NetflixSeries /> */}
+
     </>
   )
 };
 
-//Dynamic Values in JSX
+//! Dynamic Values in JSX
+//* 1: Using Variables 
+//? You can anbed any JavaScript variable within JSX by enclosing it in curly braces. The value of the variable will be inserted into the DOM at the respective location.
+
+//* 2: Expressions
+//? JSX allows you to write JS expressions inside curly braces. This includes operations, functions calls and other JavaScript expressions that produce a value.
+
+//* 3: Functions Calls
+//? Functions, especially those that return JSX, can be involved directly within your JSX.
 
 
-// Using Variables, Using Expressions
+//! Conditionals in JSX
+
 const NetflixSeries = () => {
   const name = "The Railway Men";
   const rating= "8.5 / 10";
   const summary = "After a deadly gas leaks from a factory in Bhopal, brave railway workers risk their lives to save others in the face of an unspeakable disaster.";
+  let age = 16;
 
+  const returnGenre = () =>{
+    const genre = "Thrilling & Dangerous";
+    return genre;
+  }
 
+// use conditional operator to show user can watch movie or not.
+//? 1 ---> Not recommended B/c it violet the DRY (Do not Repeat Yourself ) Rule.
+
+  // if(age<16){
+  //   return (
+  //     <div>
+  //       <div>
+  //         <img src="The Railway Man jpeg.jfif" alt="movie" width="20%" height="10%" />
+  //       </div>
+  //       <h2>Name: {name} </h2>
+  //       <h3>Year: {2020+3} </h3>
+  //       <h3>Rating: {rating} </h3>
+  //       <p>Summary: {summary} </p>
+  //       <p>Genre: {returnGenre()}</p >
+  //       <button>Not Available</button>
+  //     </div>
+  //   )
+  // };
+  
+
+//? 2 Using Ternary Operators
 
   return (
     <div>
@@ -50,10 +88,14 @@ const NetflixSeries = () => {
       <h3>Year: {2020+3} </h3>
       <h3>Rating: {rating} </h3>
       <p>Summary: {summary} </p>
+      <p>Genre: {returnGenre()}</p >
+      <button> {age >= 18 ? "Watch Now" : "Not Available"}</button>
     </div>
   )
 };
 
+
+// 07:57
 
 
 
