c742ccb04954fc3f1425b67540d19d8cc68b5503
AvinashS97 <avikumar97170@gmail.com>
2024-10-28 18:04:13 +0530

    28 Oct

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,18 +1,43 @@
 
 export const SeriesCard = (props) =>{
-    console.log(props);
+    // console.log(props);
+    
+// Without Desctructuring
+
+//     return ( 
+//           <li key={props.key}>
+//             <div>
+//               <img src={props.data.img_url} alt={props.data.name} width="40%" height="40%" />
+//             </div>
+//             <h2>Name: {props.data.name} </h2>
+//             <h3>Rating: {props.data.rating} </h3>
+//             <p> Summary: {props.data.description} </p>
+//             <p> Cast: {props.data.cast} </p>
+//             <p> Genre: {props.data.genre}</p>
+//             <a href= {props.data.watch_url} target="_blank">
+//               <button>Watch Now</button>
+//             </a>
+//             </li>)
+// }
+
+
+            {/*  After Destructuring */}
+            
+            const {id, img_url, name, rating, description, cast, genre, watch_url } = props.data; // Destructuring by storing the value from props.data...
     return ( 
-          <li key={props.key}>
+          <li key={props}>
             <div>
-              <img src={props.curElem.img_url} alt={props.curElem.name} width="40%" height="40%" />
+              <img src={img_url} alt={name} width="40%" height="40%" />
             </div>
-            <h2>Name: {props.curElem.name} </h2>
-            <h3>Rating: {props.curElem.rating} </h3>
-            <p> Summary: {props.curElem.description} </p>
-            <p> Cast: {props.curElem.cast} </p>
-            <p> Genre: {props.curElem.genre}</p>
-            <a href= {props.curElem.watch_url} target="_blank">
+            <h2>Name: {name} </h2>
+            <h3>Rating: {rating} </h3>
+            <p> Summary: {description} </p>
+            <p> Cast: {cast} </p>
+            <p> Genre: {genre}</p>
+            <a href= {watch_url} target="_blank">
               <button>Watch Now</button>
             </a>
-            </li>)
+          </li>)
 }
+
+// 05:39
