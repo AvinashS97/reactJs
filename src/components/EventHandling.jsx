5cf4ea9546249a6c7725d10bb1591bf14624da5a
AvinashS97 <avikumar97170@gmail.com>
2024-11-26 17:59:00 +0530

    Tailwind Css 15:27

--- src/components/EventHandling.jsx
+++ src/components/EventHandling.jsx
@@ -7,13 +7,24 @@ export const EventHandling = () => {
     // }
 
     //OR
-    const handleClick = () =>{
+    const handleClick = (event) =>{
+        console.log(event);
+        console.log(event.target);
+        console.log(event.type);
         alert("Hey ! I am onClick Event Handling ")
     }
 
     return (
         <>
-        <button onClick={handleClick}> Click Me</button>
+
+
+
+        {/* Function Component with named fns */}
+        {/* <button onClick={handleClick}> Click Me</button> */}
+        <br/>
+        {/* Function component with named fns using fat arrow */}
+        <button onClick={()=> handleClick(event) }>Click Me</button>
         </>
     )
 }
+// 14:42
