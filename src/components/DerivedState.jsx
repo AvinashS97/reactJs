9a6372a25757f832ea47c19678c6c98815308bea
AvinashS97 <avikumar97170@gmail.com>
2025-01-02 17:53:44 +0530

    02

--- src/components/DerivedState.jsx
+++ src/components/DerivedState.jsx
@@ -1,36 +1,35 @@
 // V29 - React Js State Challenge
 
-import { useState } from "react"
+import { useState } from 'react'
 
 export const DerivedState = () => {
+  const [users, setUsers] = useState([
+    { name: 'Avinash', age: 22 },
+    { name: 'Bob', age: 30 },
+    { name: 'Charlie', age: 35 },
+    { name: 'Angles', age: 45 }
+  ])
 
-    const [users, setUsers] = useState([
-        { name: "Avinash", age: 22 },
-        { name: "Bob", age: 30 },
-        { name: "Charlie", age: 35 },
-        { name: "Angles", age: 45 },
-    ])
+  //  Derived state:- count of users
+  console.log(users)
+  const userCount = users.length
+  const averageAge =
+    users.reduce((accum, curElem) => accum + curElem.age, 0) / userCount
 
-    //  Derived state:- count of users
-    console.log(users);
-    const userCount = users.length;
-    const averageAge = users.reduce((accum, curElem) => accum + curElem.age, 0) / userCount;
-
-    return (
-        <div className="main-div">
-            <h1>User List</h1>
-            <ul>
-                {users.map((user, index) => {
-                    return (
-                        <li key={index}>
-                            {user.name} - {user.age} years old
-                        </li>
-                    )
-                })}
-            </ul>
-            <p>Total Users:- {userCount}</p>
-            <p>Average Age:- {averageAge}</p>
-
-        </div>
-    )
+  return (
+    <div className='main-div'>
+      <h1>User List</h1>
+      <ul>
+        {users.map((user, index) => {
+          return (
+            <li key={index}>
+              {user.name} - {user.age} years old
+            </li>
+          )
+        })}
+      </ul>
+      <p>Total Users:- {userCount}</p>
+      <p>Average Age:- {averageAge}</p>
+    </div>
+  )
 }
--- src/components/hooks/ShortCircuit.jsx
+++ src/components/hooks/ShortCircuit.jsx
@@ -1,30 +1,30 @@
 // # V43- Short Circuit Evaluation
 
-import "./Hooks.css";
-import { useState } from "react";
+import './Hooks.css'
+import { useState } from 'react'
 
 const ShortCircuitExample = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const [user, setUser] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
+  const [user, setUser] = useState('')
   return (
-    <section className="container short-container">
+    <section className='container short-container'>
       <h1>Welcome to the ShortCircuit Evaluation!</h1>
 
       {/* Conditional rendering using short circuit evaluation */}
       {isLoggedIn && <p>You are logged in!</p>}
 
       {/* Another example of short circuit evaluation */}
-      {user ? `Hello ${user}` : "Plz log in!"}
+      {user ? `Hello ${user}` : 'Plz log in!'}
 
-      <div className="grid-three-cols">
+      <div className='grid-three-cols'>
         <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
           Toggle Login State
         </button>
-        <button onClick={() => setUser("Avinash")}>Set User</button>
-        <button onClick={() => setUser("")}>Clear User</button>
+        <button onClick={() => setUser('Avinash')}>Set User</button>
+        <button onClick={() => setUser('')}>Clear User</button>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default ShortCircuitExample;
+export default ShortCircuitExample
