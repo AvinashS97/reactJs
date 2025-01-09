278d83df2932aa58912548cdb48fe4a826a7c055
AvinashS97 <avikumar97170@gmail.com>
2025-01-09 14:11:12 +0530

    09Jan

--- src/components/hooks/useState/Controlled.jsx
+++ src/components/hooks/useState/Controlled.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "../Hooks.css";
 
 const ControlledForm = () => {
+    const [name, setName] = useState("");
+
+    const handleChange = (event) => {
+        setName(event.target.value);
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        // Handle with controlled state (name)
+        const dataValue = document.querySelector("#inputName").value;
+        console.log(dataValue); 
     }
 
     return (
@@ -12,7 +21,7 @@ const ControlledForm = () => {
 
             <form onSubmit={handleSubmit}>
                 <label>
-                    Name: <input id="inputName" name="name" type="text" />
+                    Name: <input id="inputName" name="name" type="text" value={name} onChange={handleChange} />
                 </label> <br />
 
                 <button type="submit" className="state-button">Submit</button>
