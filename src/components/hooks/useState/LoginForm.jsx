8f08915205bbb3391212ad552e13d4035a91585d
AvinashS97 <avikumar97170@gmail.com>
2025-01-14 17:19:40 +0530

    V51---

--- src/components/hooks/useState/LoginForm.jsx
+++ src/components/hooks/useState/LoginForm.jsx
@@ -6,26 +6,26 @@ export const LoginForm = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleFormSubmit = () => {
+    const handleFormSubmit = (e) => {
         e.preventDefault();
 
         const loginData = {
             username,
             password,
         };
-        
+
         console.log(loginData);
     };
     return (
         <div className="container">
             <div className="card">
                 <h1>Login Form</h1>
-                <form onSubmit= {handleFormSubmit}>
+                <form onSubmit={handleFormSubmit}>
                     <label htmlFor="username">Username</label>
-                    <input type="text" name="username" required autoComplete="off" value= {username} onChange={(e)=> setUsername(e.target.value)>} />
+                    <input type="text" name="username" required autoComplete="off" value={username} onChange={(e) => setUsername(e.target.value)} />
 
                     <label htmlFor="password">password</label>
-                    <input type="password" name="password" required autoComplete="off" value= {password} onChange={(e)=> setPassword(e.target.value)>} />
+                    <input type="password" name="password" required autoComplete="off" value={password} onChange={(e) => setPassword(e.target.value)} />
 
                     <button type="submit">Login</button>
                 </form>
