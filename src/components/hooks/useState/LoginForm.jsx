642c88dcb1274ed65109e61af652395170d3f6d5
AvinashS97 <128918711+AvinashS97@users.noreply.github.com>
2025-01-14 16:18:07 +0530

    V50: Create a Login Form-Done

--- src/components/hooks/useState/LoginForm.jsx
+++ src/components/hooks/useState/LoginForm.jsx
@@ -1,16 +1,31 @@
 import "./index.css";
+import { useState } from "react";
 
 export const LoginForm = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleFormSubmit = () => {
+        e.preventDefault();
+
+        const loginData = {
+            username,
+            password,
+        };
+        
+        console.log(loginData);
+    };
     return (
         <div className="container">
             <div className="card">
                 <h1>Login Form</h1>
-                <form>
+                <form onSubmit= {handleFormSubmit}>
                     <label htmlFor="username">Username</label>
-                    <input type="text" name="username" required autoComplete="off" />
+                    <input type="text" name="username" required autoComplete="off" value= {username} onChange={(e)=> setUsername(e.target.value)>} />
 
                     <label htmlFor="password">password</label>
-                    <input type="password" name="password" required autoComplete="off" />
+                    <input type="password" name="password" required autoComplete="off" value= {password} onChange={(e)=> setPassword(e.target.value)>} />
 
                     <button type="submit">Login</button>
                 </form>
