abb11e6104f71492755194ec9fe2cbe459564cb1
AvinashS97 <avikumar97170@gmail.com>
2025-01-19 18:33:09 +0530

    V52: useEffect Hook Ends

--- src/components/hooks/useEffect/index.jsx
+++ src/components/hooks/useEffect/index.jsx
@@ -4,17 +4,34 @@ import { useEffect, useState } from "react";
 import "./index.css";
 
 export const ReactUseEffect = () => {
-    const [count, setCount] = useState(0);
+    //* Example: 1
 
-    useEffect(()=> {
-        console.log("Count value is:", count);
-    }, [count]);
+    // const [count, setCount] = useState(0);
+    // useEffect(()=> {
+    //     console.log("Count value is:", count);
+    // }, [count]);
+    // return (
+    //     <div className="container effect-container">
+    //         <h1>useEffect Hook </h1>
+    //         <p>Count: {count}</p>
+    //         <button onClick={()=> setCount(count +1)}>Increment</button>
+    //     </div>
+    // );
+
+    //* Example: 2
+
+    const [date, setDate] = useState(0);
+
+    useEffect(() => {
+        setInterval(()=> {
+            const updatedDate = new Date();
+            setDate(updatedDate.toLocaleTimeString());
+        }, 1000)
+    },[]);
 
     return (
         <div className="container effect-container">
-            <h1>useEffect Hook </h1>
-            <p>Count: {count}</p>
-            <button onClick={()=> setCount(count +1)}>Increment</button>
+            <h1>Date: {date}</h1>
         </div>
     );
 };
