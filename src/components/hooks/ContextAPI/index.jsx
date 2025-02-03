4348d1b8558b8d18a6f41747a6fa63902b9ecc67
AvinashS97 <avikumar97170@gmail.com>
2025-02-03 17:01:46 +0530

    3Feb

---
+++ src/components/hooks/ContextAPI/index.jsx
@@ -0,0 +1,16 @@
+// V64.1: Context API & useContext Hook in ReactJs.
+import { createContext } from "react";
+
+// 1 Step
+export const BioContext = createContext ();
+
+//2nd Step
+
+export const BioProvider = () => {
+    const myName = "Avinash"
+    return <BioContext.Provider value={myName}>
+
+    </BioContext.Provider>
+}
+
+// 08:00
