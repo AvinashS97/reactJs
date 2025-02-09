03063c1fbed3b8bcb8195e09fe31e6632919e08d
AvinashS97 <avikumar97170@gmail.com>
2025-02-09 19:06:31 +0530

    V69 Done

--- src/hooks/ContextAPI/DarkLight.jsx
+++ src/hooks/ContextAPI/DarkLight.jsx
@@ -24,10 +24,10 @@ export const DarkLight = ()=> {
 
     const {theme, handleToggleTheme} = use(ThemeContext);
     return (
-    <div className="">
-        <h1>Dark Light Mode Website</h1>
+    <div className= { ` p-4 h-lvh flex flex-col justify-center item-center ${theme === "dark" ? "bg-gray-800" : "bg-white" } `}>
+        <h1 className= {`my-4 text-xl ${ theme === "light" ? "text-gray-800" : "text-white"}`}>Dark Light Mode Website</h1>
         <p> Hello!! My React</p>
-        <button onClick={handleToggleTheme}> 
+        <button onClick={handleToggleTheme} className="bg-blue-500 hover:bg-blue-600 mt-4 p-4 rounded-md text-white"> 
             {theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"}
         </button>
     </div>
