6534f378c3198773b4e6b960518b51b136fad74b
AvinashS97 <avikumar97170@gmail.com>
2025-02-27 16:25:07 +0530

    27Feb

--- react_router_V6/src/components/layout/Header.jsx
+++ react_router_V6/src/components/layout/Header.jsx
@@ -1,3 +1,46 @@
 export const Header = () => {
-    return <h1> Header </h1>
+    return (
+    <>
+        <header className="section-navbar">
+            <section className="top_txt">
+                <div className="head container">
+                    <div className="head_txt">
+                        <p>Get Thapa Membership, 30-day return or refund gurantee.</p>
+                    </div>
+                    <div className="sign_in_up">
+                        <a href="#">SIGN IN</a>
+                        <a href="#">SIGN UP</a>
+                    </div>
+                </div>
+            </section>
+
+            <div className="container">
+                <div className="navbar-brand">
+                    <a href="index">
+                        <p>ThapaFlix</p>
+                    </a>
+                </div>
+
+                <nav className="navbar">
+                    <ul>
+                        <li className="nav-item">
+                            <a href="/" className="nav-link">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/about" className="nav-link">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="movie" className="nav-link">Movies</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="contact" className="nav-link">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </>
+    )
 };
+
+// 06:41
--- src/source.md
+++ src/source.md
@@ -406,5 +406,7 @@ It returns an Array with exactly two values: containing the current state and a 
 # V76: React Router- createRoutesFromElements:
   * createRoutesFromElements is a helper that creates route objects form <Route> elements. It's useful if you prefer to create your routes as JSX instead of objects.
 
-# V78: APPLAYOUT in React:
+# V77: APPLAYOUT in React:
   * Outlet in React Router: An <Outlte> should be used in parent route elements to render their child route elements. This allows nested UI to show up when child routes are rendered. If the parent route matched exactly, it will render a child index route or nothing if there is no index route.
+
+# V77.1: NAVIGATION BAR in React:
