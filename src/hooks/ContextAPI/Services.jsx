435f8aabe2df8dd4e1e00324248a801b4765b681
AvinashS97 <avikumar97170@gmail.com>
2025-03-24 17:17:57 +0530

    24

--- src/hooks/ContextAPI/Services.jsx
+++ src/hooks/ContextAPI/Services.jsx
@@ -1,7 +1,7 @@
 // V65:
 //* In the Context API, the data provided by a context can only be accessed by the components that are its children within the component tree. This means that any component that needs access to the context data must be a desendant of the provider component that supplies the context Value.
 
-import { useBioContext } from '.'
+import { useBioContext } from '.';
 
 export const Services = () => {
   const { myName, myAge } = useBioContext()
